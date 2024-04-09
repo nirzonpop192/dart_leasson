0001246cc18ca0ada1c9a517c06cc1d8619a550b
“Faisal <faisal.mohammad@technonext.com>
2024-04-09 15:14:09 +0600

    first commit

---
+++ bin/comment.dart
@@ -0,0 +1,11 @@
+// Single line Comment
+/*
+  multi line comment
+  process
+ */
+/// document  comment
+/// Sets the tooltip for this radio button widget to the list of strings in
+
+void main(){
+  print("Hello world");
+}
---
+++ bin/dart_lessions.dart
@@ -0,0 +1,15 @@
+import 'package:args/args.dart';
+
+const String version = '0.0.1';
+
+
+/*
+ Documnetion comment
+ */
+/// Documnettation comment
+
+// void main(List<String> arguments) {
+//
+//   print('dart_lessions version: $version');
+//
+// }
